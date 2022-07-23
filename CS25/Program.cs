552ac0b9d4be7aededd4de23fec11a9da8565dc1
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-07-23 16:31:10 +0300

    Добавьте файлы проекта.

---
+++ CS25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -50, 6, 66, 7, 9, 10, 4, 3, 2, 1 };
+            int value = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int t = 0; t < array.Length - 1 - i; t++)
+                {
+                    if (array[t] > array[t + 1])
+                    {
+                        value = array[t];
+                        array[t] = array[t + 1];
+                        array[t + 1] = value;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
+        }
+    }
+}
